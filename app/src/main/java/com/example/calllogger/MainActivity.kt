dd7539ff155e1720682e254c5613d9cd03b69459
KevTheDev34 <k.ramirez.dev@gmail.com>
2024-10-23 15:23:29 -0700

    Set up MPAndroidChart, added ScatterChart to XML, and created phone number input and button to run script

---
+++ app/src/main/java/com/example/calllogger/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.calllogger
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var launchButton : Button
+    lateinit var phoneInput : EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+        launchButton = findViewById(R.id.launch_script_btn)
+        phoneInput = findViewById(R.id.number_input)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+}
